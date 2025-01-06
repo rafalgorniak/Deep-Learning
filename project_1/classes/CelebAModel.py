478e220f169dbbb1f388f6fb1c394fc8bc9de169
rafalgorniak <234753@edu.p.lodz.pl>
2025-01-06 22:47:34 +0100

    Added gender detecting model CNN with early stop tested on celebA

---
+++ project_1/classes/CelebAModel.py
@@ -0,0 +1,89 @@
+import torch
+
+
+class CelebAModel:
+    def __init__(self, model, criterion, optimizer, device='cpu'):
+        self.model = model.to(device)
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.device = device
+
+    def fit(self, train_loader, val_loader=None, num_epochs=10):
+        self.model.train()
+
+        patience = 5  # Number of epochs to wait before stopping
+        min_delta = 0.002  # Minimum change in loss to qualify as an improvement
+        patience_counter = 0
+        best_val_accuracy = float('inf')
+
+        for epoch in range(num_epochs):
+            running_loss = 0.0
+            for images, labels in train_loader:
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+
+                self.optimizer.zero_grad()
+
+                # Forward
+                outputs = self.model(images)
+
+                labels = labels.unsqueeze(1).float()
+                loss = self.criterion(outputs, labels)
+
+                loss.backward()
+                self.optimizer.step()
+
+                running_loss += loss.item()
+
+            print(f"Epoch {epoch+1}/{num_epochs}, Loss: {running_loss/len(train_loader):.4f}")
+
+            if val_loader:
+                val_accuracy = self.evaluate(val_loader)
+                print(f"Validation Accuracy: {val_accuracy * 100:.2f}%")
+
+                if val_accuracy > best_val_accuracy + min_delta:
+                    best_val_accuracy = val_accuracy
+                    patience_counter = 0
+                else:
+                    patience_counter += 1
+                    if patience_counter >= patience:
+                        print("Early stopping triggered.")
+                        break
+
+    def predict(self, test_loader):
+        self.model.eval()
+        predictions = []
+        true_labels = []
+
+        with torch.no_grad():
+            for images, labels in test_loader:
+                images = images.to(self.device)
+                labels = labels.to(self.device)
+
+                # Forward
+                outputs = self.model(images)
+                predictions.append(torch.sigmoid(outputs).cpu())
+                true_labels.append(labels.cpu())
+
+        # Zwracamy predykcje i prawdziwe etykiety jako tensory
+        return torch.cat(predictions), torch.cat(true_labels)
+
+    def evaluate(self, data_loader):
+        self.model.eval()
+        correct = 0
+        total = 0
+
+        with torch.no_grad():
+            for images, labels in data_loader:
+                images = images.to(self.device)
+                labels = labels.to(self.device).unsqueeze(1).float()  # Ensure shape compatibility
+
+                # Forward pass
+                outputs = self.model(images)
+                predictions = (torch.sigmoid(outputs) > 0.5).float()  # Convert to 0/1
+
+                # Accuracy calculation
+                correct += (predictions == labels).sum().item()
+                total += labels.size(0)  # Count total samples
+
+        return correct / total  # Return accuracy as a fraction
--- project_1/data/data_loader.py
+++ project_1/data/data_loader.py
@@ -1,9 +1,9 @@
+import numpy as np
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 
-def load_celeba_dataset(data_dir, batch_size=32, image_size=64, split='train'):
-
+def load_celeba_dataset(data_dir, batch_size=32, image_size=64, split='train', subset_size=None):
     # Transformacje dla obrazów, zmiana rozdzielczości i jakaś normalizacja do wartości [-1,1]
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
@@ -11,24 +11,27 @@ def load_celeba_dataset(data_dir, batch_size=32, image_size=64, split='train'):
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
 
-
+    def target_transform(target):
+        return target[20].item()
 
     # Pobieranie zbioru CelebA, możliwe 3 opcje: train, test oraz validacyjny
     dataset = datasets.CelebA(
         root=data_dir,
         split=split,
         transform=transform,
+        target_transform=target_transform,
         download=True
     )
 
-    # Tworzenie DataLoadera, ustaiwanie rozmiaru paczek oraz tasowanie
+    if subset_size is not None:
+        dataset = reduce_data_number(dataset, subset_size)
+
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     return data_loader
 
 
-def load_wiederface_dataset(data_dir, batch_size=32, image_size=64, split='train'):
-
+def load_wiederface_dataset(data_dir, batch_size=32, image_size=64, split='train', subset_size=None):
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
@@ -42,21 +45,35 @@ def load_wiederface_dataset(data_dir, batch_size=32, image_size=64, split='train
         download=True
     )
 
-    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    if subset_size is not None:
+        dataset = reduce_data_number(dataset, subset_size)
+
+    data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
     return data_loader
 
 
-def load_dataset(dataset):
+def reduce_data_number(data_set, subset_size):
+    total_samples = len(data_set)
+    subset_size = int(total_samples * subset_size)
+
+    selected_indices = np.random.choice(total_samples, subset_size, replace=False)
+    reduced_subset = Subset(data_set, selected_indices)
+
+    return reduced_subset
+
+
+def load_dataset(dataset, subset_size=1):
     if dataset == 'CelebA':
-        train_loader = load_celeba_dataset(data_dir="./data", batch_size=64, split='train')
-        valid_loader = load_celeba_dataset(data_dir="./data", batch_size=64, split='valid')
-        test_loader = load_celeba_dataset(data_dir="./data", batch_size=64, split='test')
+        train_loader = load_celeba_dataset(data_dir="./data", batch_size=64, split='train', subset_size=subset_size)
+        valid_loader = load_celeba_dataset(data_dir="./data", batch_size=64, split='valid', subset_size=subset_size)
+        test_loader = load_celeba_dataset(data_dir="./data", batch_size=64, split='test', subset_size=subset_size)
     elif dataset == 'WIDERFace':
-        train_loader = load_wiederface_dataset(data_dir="./data", batch_size=64, split='train')
-        valid_loader = load_wiederface_dataset(data_dir="./data", batch_size=64, split='val')
-        test_loader = load_wiederface_dataset(data_dir="./data", batch_size=64, split='test')
+        train_loader = load_wiederface_dataset(data_dir="./data", batch_size=64, split='train', subset_size=subset_size)
+        valid_loader = load_wiederface_dataset(data_dir="./data", batch_size=64, split='val', subset_size=subset_size)
+        test_loader = load_wiederface_dataset(data_dir="./data", batch_size=64, split='test', subset_size=subset_size)
     else:
         raise NotImplementedError
 
     return train_loader, valid_loader, test_loader
+
