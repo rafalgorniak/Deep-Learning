1022873a114d9f07793896d3f18e3a5fef91d622
Piotr <hynas321@gmail.com>
2025-01-12 13:33:59 +0100

    Set droput rate as a parameter

--- project_2/SymbolLSTMModel.py
+++ project_2/SymbolLSTMModel.py
@@ -2,14 +2,14 @@ from torch import nn
 
 
 class SymbolLSTMModel(nn.Module):
-    def __init__(self, unique_symbols_count: int, embedding_dimensions: int, hidden_dimensions: int, num_layers: int=1):
+    def __init__(self, unique_symbols_count: int, embedding_dimensions: int, hidden_dimensions: int, num_layers: int=1, droput_rate: float = 0.2):
         super(SymbolLSTMModel, self).__init__()
         self.embedding = nn.Embedding(unique_symbols_count, embedding_dimensions)
         self.lstm = nn.LSTM(input_size=embedding_dimensions,
                             hidden_size=hidden_dimensions,
                             num_layers=num_layers,
                             batch_first=True)
-        self.dropout = nn.Dropout(0.2)
+        self.dropout = nn.Dropout(droput_rate)
         self.fc = nn.Linear(hidden_dimensions, unique_symbols_count)
         
     def forward(self, x, hidden=None):
